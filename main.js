2e8e26d93435378aee4a326dd7ba3a721ab05712
Felix Chin <felixmchin@gmail.com>
2021-01-03 22:35:18 -0800

    updates to results page

--- main.js
+++ main.js
@@ -4,24 +4,6 @@ let players = [];
 let matches = [];
 let allMatches = [];
 let matchReports= [];
-let match1HomeTeam = '';
-let match1AwayTeam = '';
-let match2HomeTeam = '';
-let match2AwayTeam = '';
-let match3HomeTeam = '';
-let match3AwayTeam = '';
-let match4HomeTeam = '';
-let match4AwayTeam = '';
-let match1HomeScore = null;
-let match1AwayScore = null;
-let match2HomeScore = null;
-let match2AwayScore = null;
-let match3HomeScore = null;
-let match3AwayScore = null;
-let match4HomeScore = null;
-let match4AwayScore = null;
-const date = new Date();
-const currentDate = new Date(date.getTime() - (12*60*60*1000) - (date.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
 const chooseTeamHeading = document.querySelector('.choose-team-heading');
 const teamLogos = document.querySelector('.team-logos-wrapper');
 const playersTable = document.querySelector('.players-table');
@@ -34,14 +16,6 @@ const stadium = document.querySelector('.venue-name');
 const website = document.querySelector('.website');
 const logoWrapper = document.querySelector('.logo-wrapper');
 const homeButton = document.querySelector('.home-button');
-const match1Home = document.querySelector('.match1Home');
-const match1Away = document.querySelector('.match1Away');
-const match2Home = document.querySelector('.match2Home');
-const match2Away = document.querySelector('.match2Away');
-const match3Home = document.querySelector('.match3Home');
-const match3Away = document.querySelector('.match3Away');
-const match4Home = document.querySelector('.match4Home');
-const match4Away = document.querySelector('.match4Away');
 const articleGroup = document.querySelector('.article-group');
 const loader = document.querySelector('.loading-screen');
 const highlights = document.querySelector('.highlights');
@@ -116,7 +90,6 @@ function onLogoClick(event) {
   )
   displayClubHeader();
   getMatchInfo();
-  // getMatchResults();
   getPlayers(teams[teamSelected]['id']);
 }
 
@@ -160,27 +133,15 @@ function reset() {
   teamDetails.classList.add('d-none');
   detailsHeader.classList.add('d-none');
   menuBar.classList.add('d-none');
-  // match1Home.style.color = '';
-  // match1Home.style.fontWeight = '';
-  // match1Away.style.color = '';
-  // match1Away.style.fontWeight = '';
-  // match2Home.style.color = '';
-  // match2Home.style.fontWeight = '';
-  // match2Away.style.color = '';
-  // match2Away.style.fontWeight = '';
-  // match3Home.style.color = '';
-  // match3Home.style.fontWeight = '';
-  // match3Away.style.color = '';
-  // match3Away.style.fontWeight = '';
-  // match4Home.style.color = '';
-  // match4Home.style.fontWeight = '';
-  // match4Away.style.color = '';
-  // match4Away.style.fontWeight = '';
   matchReports = [];
   players = [];
   playersTable.innerHTML = '';
   logoWrapper.innerHTML = '';
   results.innerHTML = '';
+  news.classList.remove('d-none');
+  matchResults.classList.add('d-none');
+  stats.classList.add('d-none');
+  roster.classList.add('d-none');
 }
 
 function getMatchInfo() {
@@ -189,7 +150,8 @@ function getMatchInfo() {
     const span1 = document.createElement('span');
     const span2 = document.createElement('span');
     const span3 = document.createElement('span');
-    const date = matches[i].utcDate.split('T')[0];
+    const options = { day: 'numeric', month: 'short', year: 'numeric' }
+    const date = new Date(matches[i].utcDate.split('T')[0]).toLocaleDateString('en-US', options);
     const score = `${matches[i].score.fullTime.homeTeam} - ${matches[i].score.fullTime.awayTeam}`;
     let opp = '';
     if (matches[i].homeTeam.name !== teams[teamSelected].name) {
@@ -200,75 +162,15 @@ function getMatchInfo() {
     span1.textContent = date;
     span2.textContent = opp;
     span3.textContent = score;
+    div.classList.add('row')
+    span1.classList.add('col-4');
+    span2.classList.add('col-auto');
+    span3.classList.add('col-2');
     div.append(span1, span2, span3);
     results.appendChild(div);
   }
   getNewsArticle();
 }
-
-
-// function colorScore() {
-//   if(match1HomeScore > match1AwayScore) {
-//     match1Home.style.color = '#E4B222';
-//     match1Home.style.fontWeight = 'bold';
-//   } else if (match1AwayScore > match1HomeScore) {
-//     match1Away.style.color = '#E4B222';
-//     match1Away.style.fontWeight = 'bold';
-//   } else if (match1AwayScore === match1HomeScore) {
-//     match1Away.style.fontWeight = 'bold';
-//     match1Home.style.fontWeight = 'bold';
-//   }
-//   if (match2HomeScore > match2AwayScore) {
-//     match2Home.style.color = '#E4B222';
-//     match2Home.style.fontWeight = 'bold';
-//   } else if (match2AwayScore > match2HomeScore) {
-//     match2Away.style.color = '#E4B222';
-//     match2Away.style.fontWeight = 'bold';
-//   } else if (match2AwayScore === match2HomeScore) {
-//     match2Away.style.fontWeight = 'bold';
-//     match2Home.style.fontWeight = 'bold';
-//   }
-//   if (match3HomeScore > match3AwayScore) {
-//     match3Home.style.color = '#E4B222';
-//     match3Home.style.fontWeight = 'bold';
-//   } else if (match3AwayScore > match3HomeScore) {
-//     match3Away.style.color = '#E4B222';
-//     match3Away.style.fontWeight = 'bold';
-//   } else if (match3AwayScore === match3HomeScore) {
-//     match3Away.style.fontWeight = 'bold';
-//     match3Home.style.fontWeight = 'bold';
-//   }
-//   if (match4HomeScore > match4AwayScore) {
-//     match4Home.style.color = '#E4B222';
-//     match4Home.style.fontWeight = 'bold';
-//   } else if (match4AwayScore > match4HomeScore) {
-//     match4Away.style.color = '#E4B222';
-//     match4Away.style.fontWeight = 'bold';
-//   } else if (match4AwayScore === match4HomeScore) {
-//     match4Away.style.fontWeight = 'bold';
-//     match4Home.style.fontWeight = 'bold';
-//   }
-// }
-
-// function getMatchResults() {
-//   match1HomeScore = matches[matches.length - 1]['score']['fullTime']['homeTeam'];
-//   match1AwayScore = matches[matches.length - 1]['score']['fullTime']['awayTeam'];
-//   match2HomeScore = matches[matches.length - 2]['score']['fullTime']['homeTeam'];
-//   match2AwayScore = matches[matches.length - 2]['score']['fullTime']['awayTeam'];
-//   match3HomeScore = matches[matches.length - 3]['score']['fullTime']['homeTeam'];
-//   match3AwayScore = matches[matches.length - 3]['score']['fullTime']['awayTeam'];
-//   match4HomeScore = matches[matches.length - 4]['score']['fullTime']['homeTeam'];
-//   match4AwayScore = matches[matches.length - 4]['score']['fullTime']['awayTeam'];
-//
-//   match1Home.textContent = `${match1HomeTeam} ${match1HomeScore}`;
-//   match1Away.textContent = `${match1AwayScore} ${match1AwayTeam}`;
-//   match2Home.textContent = `${match2HomeTeam} ${match2HomeScore}`;
-//   match2Away.textContent = `${match2AwayScore} ${match2AwayTeam}`;
-//   match3Home.textContent = `${match3HomeTeam} ${match3HomeScore}`;
-//   match3Away.textContent = `${match3AwayScore} ${match3AwayTeam}`;
-//   match4Home.textContent = `${match4HomeTeam} ${match4HomeScore}`;
-//   match4Away.textContent = `${match4AwayScore} ${match4AwayTeam}`;
-// }
 
 function handleGetError(error) {
   console.error(error);
